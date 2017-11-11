fb788230a95c150a6e1bbd39a0a6ab7aa9920866
Karthick Eswar <karthickeswar@virtualgodown.com>
2017-11-11 19:25:05 +0530

    app

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ if (!app.get('port')) {
   app.set('port', 3000);
 }
 app.use("/", express.static(path.join(__dirname, 'public')));
-app.use("/*",function(req,res,next){
-  res.sendFile(path.join(__dirname, '/public/html/index.html'));
-})
+app.use("/*", function(req, res, next) {
+  console.log(req.get('host'));
+  var host=req.get('host') || req.get('origin')
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    res.redirect("https://"+host + req.url);
+  } else {
+    res.sendFile(path.join(__dirname, '/public/html/index.html'));
+  }
+});
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
